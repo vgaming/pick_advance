e7fec52617c04bc3c2e4a11014c9fa122869cf55
Vasya N <n1m5spm@yandex.com>
2018-03-08 11:40:20 +0300

    working version

--- lua/main.lua
+++ lua/main.lua
@@ -1,18 +1,18 @@
 -- << pick_advancement_vasya_main.lua
 
 local pickadvance = pickadvance
-local wesnoth = wesnoth
 local assert = assert
 local ipairs = ipairs
+local print_as_json = print_as_json
 local string = string
 local table = table
+local wesnoth = wesnoth
 local T = wesnoth.require("lua/helper.lua").set_wml_tag_metatable {}
-
---wesnoth.dofile("~add-ons/Creep_War_Dev/lua/.vasya_personal_json_format.lua") -- TODO
 
 
 wesnoth.wml_actions.event {
 	id = "pickadvance_side_turn_end",
+	first_time_only = false,
 	name = "side turn end",
 	T.lua { code = "pickadvance.side_turn_end()" }
 }
@@ -86,12 +86,12 @@ local function apply_advances_config(unit, force)
 			local parsed = parse_advances_config_local_function(unit)
 			return { value = table.concat(parsed, ",") }
 		end).value
-		assert(string.find(wesnoth.unit_types[unit.type].__cfg.advances_to or "", user_advances),
+		assert(string.find(table.concat(pickadvance.advance_array(unit.type), ","), user_advances),
 			"Chosen advancement not found for unit type. Please report if you see this.")
 		user_advances = split_comma_units(user_advances)
 		unit.advances_to = user_advances
 		unit.variables.pickadvance_handled = true
-		--print_as_json("advances for", unit.type, "x", unit.x, "y", unit.y, "advance_array", unit.advances_to, user_advances)
+		print_as_json("applied advance for", unit.type, "x", unit.x, "y", unit.y, "advance_array", unit.advances_to, user_advances)
 	end
 end
 
@@ -105,6 +105,7 @@ end
 
 
 function pickadvance.side_turn_end()
+	print_as_json("Handling side turn end", wesnoth.current.side)
 	for _, unit in ipairs(wesnoth.get_units { side = wesnoth.current.side }) do
 		apply_advances_config(unit, false)
 	end
@@ -117,7 +118,7 @@ function pickadvance.pick_advance()
 	local unit = wesnoth.get_unit(x1, y1)
 	wesnoth.synchronize_choice(function()
 		local dialog_result = pickadvance.show_dialog_unsynchronized(unit)
-		--print_as_json("chosen advances for unit", unit.x, unit.y, "is", dialog_result)
+		print_as_json("locally chosen advance for unit", unit.type, unit.x, unit.y, dialog_result)
 		if dialog_result.is_ok then
 			save_user_preferences(unit, dialog_result)
 		end
