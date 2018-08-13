6fe36fa7a97260810ee7bf031e5342af08b8fdd9
Vasya N <n1m5spm@yandex.com>
2018-08-14 00:04:22 +0300

    clean up

--- .build/config.sh
+++ .build/config.sh
@@ -8,6 +8,6 @@ icon() {
 	arrow="lobby/status-lobby-s.png~SCALE(36,36)"
 	marksman="units/elves-wood/marksman.png~CROP(16,12,47,47)~SCALE(36,36)"
 	avenger="units/elves-wood/avenger.png~CROP(20,12,47,47)~SCALE(36,36)"
-	echo "$hex~BLIT($hex)~BLIT($arrow,0,36)~BLIT($avenger,36,0)~BLIT($marksman,36,36)"
+	echo "$hex~BLIT($arrow,0,36)~BLIT($avenger,36,0)~BLIT($marksman,36,36)"
 }
 addon_manager_args=("--pbl-key" "icon" "$(icon)")
